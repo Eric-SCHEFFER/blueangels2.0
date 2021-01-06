06bae620e41d8411b8b9488a55101d9d5d1155e0
eric <eric.scheffer.pro@gmail.com>
2021-01-06 16:00:03 +0100

    Factorisé HomeController (mis partie code dans methode private)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,21 +22,33 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        // On recupère jusqu'à 3 events futurs
-        // $countEventsToCome = nombre d'évènements récupérés
-        // S'il y a moins de 3 events futurs (Si $countEventsToCome<3), on récupère aussi 3-$countEventsToCome évènements passés
-        $date  = mktime(0, 0, 0, date("m")  , date("d") -1, date("Y")); // Pour tester une autre date
-        $today = date('Y-m-d',$date);
+        $events = $this->getEvents();
+        return $this->render('home.html.twig', [
+            'eventsToCome' => $events[0],
+            'completedEvents' => $events[1],
+            'today' => $events[2],
+        ]);
+    }
+
+
+
+    // On recupère jusqu'à 3 events futurs
+    // $countEventsToCome = nombre d'évènements récupérés
+    // S'il y a moins de 3 events futurs (Si $countEventsToCome<3), on récupère aussi 3-$countEventsToCome évènements passés
+    private function getEvents()
+    {
+        $date  = mktime(0, 0, 0, date("m"), date("d") - 1, date("Y")); // Pour tester une autre date
+        $today = date('Y-m-d', $date);
         $eventsToCome = $this->eventsRepository->findEventsToCome($today);
         $completedEvents = [];
         $countEventsToCome = count($eventsToCome);
         if ($countEventsToCome < 3) {
             $completedEvents = $this->eventsRepository->findCompletedEvents($today, 3 - $countEventsToCome);
         }
-        return $this->render('home.html.twig', [
-            'eventsToCome' => $eventsToCome,
-            'completedEvents' => $completedEvents,
-            'today' => $today,
-        ]);
+        return [
+            $eventsToCome,
+            $completedEvents,
+            $today
+        ];
     }
 }
