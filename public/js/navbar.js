07554c48e879320e05d9b351f4e271b0e6f91a6c
eric <eric.scheffer.pro@gmail.com>
2021-02-15 18:25:38 +0100

    front: Menu dropdown on click

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,2 +1,13 @@
-
+function developperMenu() {
+   document.getElementById("dropdown").classList.toggle("show");
+ }
  
+ // Close the dropdown if the user clicks outside of it
+ window.onclick = function(e) {
+   if (!e.target.matches('.btDropdown')) {
+   let dropdown = document.getElementById("dropdown");
+     if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+     }
+   }
+ }
