f775a2d1b6eb62ef429a117171a1340bdda5672d
eric <eric.scheffer.pro@gmail.com>
2021-02-17 10:51:02 +0100

    front: Menu dropdown on click responsive

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,18 +1,18 @@
-// On ouvre ou ferme un sous-menu au clic sur son bouton
+// Au clic du bouton, on ouvre/ferme son sous-menu 
 function toggleMenu(iDMenu) {
    let dropDownTarget = document.getElementById(iDMenu);
    let dropdowns = document.getElementsByClassName('dropdown-content');
    let i;
    for (i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
-      // On ferme tous les menus dropdown ouvert, exepté celui du bouton cliqué
+      // On ferme tous les menus dropdown ouvert, excepté celui du bouton cliqué
       if (openDropdown != dropDownTarget) {
          if (openDropdown.classList.contains('show')) {
             openDropdown.classList.remove('show');
          }
       }
    }
-   // Bascule On/Off du menu dropdown au clic sur le bouton
+   // Bascule On/Off du menu dropdown au clic du le bouton
    dropDownTarget.classList.toggle("show");
  }
 
