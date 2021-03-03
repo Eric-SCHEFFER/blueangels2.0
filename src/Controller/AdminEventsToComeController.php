37df9bb3d5691b41738b914e426fbd4abc83341f
eric <eric.scheffer.pro@gmail.com>
2021-03-03 19:27:43 +0100

    admin events a venir: Editer images multiples

--- src/Controller/AdminEventsToComeController.php
+++ src/Controller/AdminEventsToComeController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Events;
+use App\Entity\ImagesEvent;
 use App\Repository\EventsRepository;
 use App\Service\TodayGenerator;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\EventType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminEventsToComeController extends AbstractController
 {
@@ -74,17 +76,44 @@ class AdminEventsToComeController extends AbstractController
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+
+            // On récupère les images transmises
+            $images = $form->get('imageFile')->getData();
+            // On boucle sur les images
+            foreach ($images as $image) {
+                // On génère un nouveau nom de fichier
+                $ext = $image->guessExtension();
+                $fichier = md5(uniqid()) . '.' . $ext;
+                // On copie le fichier dans le dossier uploads
+                $dossierImages = $this->getParameter('dossier_images_events');
+                $image->move(
+                    $dossierImages,
+                    $fichier
+                );
+
+                $imageSource = $dossierImages . "/" . $fichier;
+                $imageCible = $dossierImages . "/min_" . $fichier;
+                // On créé une miniature du fichier image. En 3e paramètre, la largeur souhaitée en px de la miniature
+                $this->creeMiniature($imageSource, $imageCible, 290);
+
+                // On stocke le nom de l'image dans la base de données
+                $img = new ImagesEvent();
+                $img->setNom($fichier);
+                $event->addImagesEvent($img);
+            }
+
             $this->em->persist($event);
             $this->em->flush();
             $this->addFlash('succes', 'Évènement mis à jour avec succès');
             return $this->redirectToRoute('admin.events_a_venir');
         }
         return $this->render('admin/events/edit.html.twig', [
+            'event' => $event,
             'form' => $form->createView()
         ]);
     }
 
-    // ======== SUPPRIMER UN ÉVÈNEMENT ========
+    // ======== SUPPRIMER UN ÉVÈNEMENT ET SES IMAGES ========
     /**
      * @Route("/admin/events/edit/{id}", name="admin.event.avenir.delete", methods={"DELETE"})
      * @param Events $events
@@ -94,7 +123,19 @@ class AdminEventsToComeController extends AbstractController
     {
         // Vérif token pour sécuriser la suppression d'une réalisation
         if ($this->isCsrfTokenValid('delete' . $event->getId(), $request->get('_token'))) {
-            // Vérif token pour sécuriser la suppression d'une réalisation
+
+            // Rechercher dans Images toutes les images de la réalisation en cours
+            $images = $this->getDoctrine()->getRepository(ImagesEvent::class)->findBy(
+                ['event' => $event->getId()]
+            );
+            // On récupére dans une boucle le nom de chaque image, et on la supprime sur le disque
+            foreach ($images as $image) {
+                $nom = $image->getNom();
+                unlink($this->getParameter('dossier_images_events') . '/' . $nom);
+                // On supprime également le fichier miniature
+                unlink($this->getParameter('dossier_images_events') . '/min_' . $nom);
+            }
+
             // On supprime la réalisation, ainsi que toutes ses images (Option orphanRemoval) dans la base
             $this->em->remove($event);
             $this->em->flush();
@@ -102,5 +143,83 @@ class AdminEventsToComeController extends AbstractController
             //return new HttpFoundationResponse('Suppression');
         }
         return $this->redirectToRoute('admin.events_a_venir');
+    }
+
+    // ======== SUPPRIMER UNE IMAGE ========
+    /**
+     * @route("/admin/events/image/supprime{id}", name="admin.events.image.delete", methods={"DELETE"})
+     */
+    public function deleteImage(ImagesEvent $imagesEvent, Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        // On vérifie si le token est valide
+        if ($this->isCsrfTokenValid('delete' . $imagesEvent->getId(), $data['_token'])) {
+            // On récupère le nom de l'image (le champ "lien" n'est peut-être pas un bon nom, j'en suis conscient)
+            $nom = $imagesEvent->getNom();
+            // On supprime le fichier
+            unlink($this->getParameter('dossier_images_events') . '/' . $nom);
+            // On supprime également le fichier miniature
+            unlink($this->getParameter('dossier_images_events') . '/min_' . $nom);
+            // On supprime le nom de l'image de la base de données
+            $this->em->remove($imagesEvent);
+            $this->em->flush();
+
+            // On répond en json
+            return new JsonResponse(['success' => 1]);
+        } else {
+            return new JsonResponse(['error' => 'Token Invalide'], 400);
+        }
+    }
+
+    // ======== CRÉÉ UNE MINIATURE D'UNE IMAGE =========
+    // En entrée, le chemin complet d'un fichier image jpg ou png
+    private function creeMiniature($imageSource, $imageCible, $targetWidth)
+    {
+        // On recupère l'extension, et on minimise les caractères
+        $ext = strtolower(pathinfo($imageSource, PATHINFO_EXTENSION));
+        // On stocke dans des variables les noms des fonctions à lancer plus tard, selon l'extension de l'image
+        if ($ext == "jpg" || $ext == "jpeg") {
+            $imagecreatefrom = "imagecreatefromjpeg";
+            $imageSortie = "imagejpeg";
+        } elseif ($ext == "png") {
+            $imagecreatefrom = "imagecreatefrompng";
+            $imageSortie = "imagepng";
+        } else {
+            // On retourne une erreur, car ce n'est ni une image jpg, ni png
+            return "Image non valide (jpg ou png uniquement)";
+        }
+
+        $sourceSize = getimagesize($imageSource);
+        $portraitMalOriente = false;
+        // On détecte si une image jpg est en portrait, et si elle est mal orientée
+        if ($imageSortie == "imagejpeg") {
+            // dd(exif_read_data($imageSource, 'ANY_TAG'));
+            if (isset(exif_read_data($imageSource, 'ANY_TAG')['Orientation'])) {
+                $portraitMalOriente = exif_read_data($imageSource, 'ANY_TAG')['Orientation'];
+                if ($portraitMalOriente == 6 && $sourceSize[0] > $sourceSize[1]) {
+                    $portraitMalOriente = true;
+                } else {
+                    $portraitMalOriente = false;
+                }
+            }
+        }
+        if ($portraitMalOriente) {
+            $sourceWidth = $sourceSize[1];
+            $sourceHeight = $sourceSize[0];
+        } else {
+            $sourceWidth = $sourceSize[0];
+            $sourceHeight = $sourceSize[1];
+        }
+        // On calcule les dimensions de la miniature, et on lance les fonctions php de création de miniature
+        $targetHeight = ($targetWidth / $sourceWidth) * $sourceHeight;
+        $imgIn = $imagecreatefrom($imageSource);
+        // On pivote l'image de 90° dans le sens horaire, si nécéssaire
+        if ($portraitMalOriente) {
+            $imgIn = imagerotate($imgIn, -90, 0);
+        }
+        $imgOut = imagecreatetruecolor($targetWidth, $targetHeight);
+        imagecopyresampled($imgOut, $imgIn, 0, 0, 0, 0, $targetWidth, $targetHeight, $sourceWidth, $sourceHeight);
+        $imageSortie($imgOut, $imageCible);
+        return $imageCible;
     }
 }
