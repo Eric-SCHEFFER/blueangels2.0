374e1ec3f4cec19891874d5dbd59f169185e9a48
eric <eric.scheffer.pro@gmail.com>
2021-07-23 19:19:14 +0200

    Front: _eventsBase.html.twig: Affiche demain au lieu de dans 1 jour

--- src/Twig/AutoLinkTwigExtension.php
+++ src/Twig/AutoLinkTwigExtension.php
@@ -18,7 +18,7 @@ class AutoLinkTwigExtension extends AbstractExtension
     {
         // Pattern de départ avant modif (ajout double point et underscore qui n'était pas compris comme inclus dans un url)
         // $pattern = "/http[s]?:\/\/[a-zA-Z0-9.\-\/?#=&]+/";
-        $pattern = "/http[s]?:\/\/[a-zA-Z0-9.:_%()\-\/?#=&]+/";
+        $pattern = "/http[s]?:\/\/[a-zA-Z0-9.@:_%()\-\/?#=&]+/";
         $replacement = "<a href=\"$0\" target=\"_blank\">$0</a>";
         $string = preg_replace($pattern, $replacement, $string);
         return $string;
