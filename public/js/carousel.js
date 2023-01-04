f227237963853195b42e54fbfcb2972d3a59b59d
eric <eric.scheffer.pro@gmail.com>
2023-01-04 19:36:11 +0100

    slide image uniquement vers in: OK

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -20,12 +20,27 @@ if (bullet.length > 0) {
       if (n > slides.length) { slideIndex = 1 }
       if (n < 1) { slideIndex = slides.length }
       for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
+
+         // TODO: Transitions pour l'image
+         // Ne fonctionne pour l'instant qu'à l'apparition
+
+
+         slides[i].classList.add("display-image-off");
+         slides[i].classList.add("visibility-image-off");
+         slides[i].classList.remove("display-image-on");
+         slides[i].classList.remove("visibility-image-on");
+
       }
       for (i = 0; i < bullets.length; i++) {
          bullets[i].className = bullets[i].className.replace(" actif", "");
       }
-      slides[slideIndex - 1].style.display = "block";
+      // TODO: Transitions pour l'image
+
+      slides[slideIndex - 1].classList.remove("display-image-off");
+      slides[slideIndex - 1].classList.add("visibility-image-on");
+      slides[slideIndex - 1].classList.add("display-image-on");
+      slides[slideIndex - 1].classList.remove("visibility-image-off");
+
       bullets[slideIndex - 1].className += " actif";
    }
 }
