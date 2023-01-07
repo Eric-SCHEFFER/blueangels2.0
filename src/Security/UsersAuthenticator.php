c7a0216ee3a12cc0a55df57696bf6ddd4edb7f3a
eric <eric.scheffer.pro@gmail.com>
2023-01-07 18:29:10 +0100

    update

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -71,7 +71,7 @@ class UsersAuthenticator extends AbstractFormLoginAuthenticator implements Passw
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('mdp invalide');
         }
 
         return $user;
