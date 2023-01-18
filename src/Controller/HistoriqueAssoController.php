aca93d8de5d0095595e6eac59f6bc85a799eb2b4
eric <eric.scheffer.pro@gmail.com>
2023-01-18 21:12:04 +0100

    Corrigé erreur variable twig inexistante en modifiant certains controlleurs du menu, qui dirigent maintenant vers la catégorie au lieu de l'article.

--- src/Controller/HistoriqueAssoController.php
+++ src/Controller/HistoriqueAssoController.php
@@ -5,37 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Articles;
-use App\Entity\CategoriesArticle;
-// use App\Repository\ImagesArticleRepository;
+use App\Repository\ArticlesRepository;
+use App\Repository\CategoriesArticleRepository;
 
 class HistoriqueAssoController extends AbstractController
 {
+    public function __construct(
+        ArticlesRepository $articlesRepository,
+        CategoriesArticleRepository $categoriesArticleRepository
+    ) {
+        $this->articlesRepository = $articlesRepository;
+        $this->categoriesArticleRepository = $categoriesArticleRepository;
+    }
+
     /**
-     * @Route("/historiqueAsso/", name="historique.asso")
+     * @Route("/historiqueAsso", name="historique.asso")
      */
-    public function findFirstArticleByCategorie(): Response
+    public function loadHistoriqueAsso(): Response
     {
-        $repoArticles = $this->getDoctrine()->getRepository(Articles::class);
-        $repoCat = $this->getDoctrine()->getRepository(CategoriesArticle::class);
-        // On recherche dans CategoriesArticle, la catégorie "Historique asso".  
-        $categorie = $repoCat->findOneBy(array('nom' => 'Historique asso'));
-        // On cherche l'article actif le plus récent dans cette catégorie
-        $findArticle = $repoArticles->findOneBy(
-            [
-                'categories_article' => $categorie,
-                'actif' => true,
-            ],
-            ['created_at' => "DESC"]
-        );
-        if (empty($findArticle)) {
-            // Erreur 404
-            throw $this->createNotFoundException('L\'article est introuvable');
-        }
-        $id = $findArticle->getId();
-        $article = $repoArticles->find($id);
-        return $this->render('article/index.html.twig', [
-            'article' => $article,
-        ]);
+        $categorie = $this->categoriesArticleRepository->findOneBy(['nom' => 'Historique asso']);
+        $categorieId = $categorie->getId();
+        // On va sur la route qui affiche les articles selon la catégorie passée en id, et c'est sur ArticlesByCategorieController que s'effectue la logique
+        return $this->redirectToRoute('categorie.articles', ['idCategorie' => $categorieId]);
     }
 }
--- src/Controller/StagesController.php
+++ src/Controller/StagesController.php
@@ -5,37 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Articles;
-use App\Entity\CategoriesArticle;
+use App\Repository\ArticlesRepository;
+use App\Repository\CategoriesArticleRepository;
 
 class StagesController extends AbstractController
 {
+    public function __construct(
+        ArticlesRepository $articlesRepository,
+        CategoriesArticleRepository $categoriesArticleRepository
+    ) {
+        $this->articlesRepository = $articlesRepository;
+        $this->categoriesArticleRepository = $categoriesArticleRepository;
+    }
+
     /**
-     * @Route("/stages/", name="stages")
+     * @Route("/stages", name="stages")
      */
-    public function findFirstArticleByCategorie(): Response
+    public function loadStages(): Response
     {
-        $repoArticles = $this->getDoctrine()->getRepository(Articles::class);
-        $repoCat = $this->getDoctrine()->getRepository(CategoriesArticle::class);
-        // On recherche dans CategoriesArticle, la catégorie "Stages".  
-        $categorie = $repoCat->findOneBy(array('nom' => 'Stages'));
-        // On cherche l'article actif le plus récent dans cette catégorie
-        $findArticle = $repoArticles->findOneBy(
-            [
-                'categories_article' => $categorie,
-                'actif' => true,
-            ],
-            ['created_at' => "DESC"]
-        );
-        if (empty($findArticle)) {
-            // Erreur 404
-            throw $this->createNotFoundException('L\'article est introuvable');
-        }
-        $id = $findArticle->getId();
-        $article = $repoArticles->find($id);
-        return $this->render('article/index.html.twig', [
-            'article' => $article,
-            'menu_courant' => 'stages',
-        ]);
+        $categorie = $this->categoriesArticleRepository->findOneBy(['nom' => 'Stages']);
+        $categorieId = $categorie->getId();
+        // On va sur la route qui affiche les articles selon la catégorie passée en id, et c'est sur ArticlesByCategorieController que s'effectue la logique
+        return $this->redirectToRoute('categorie.articles', ['idCategorie' => $categorieId]);
     }
 }
