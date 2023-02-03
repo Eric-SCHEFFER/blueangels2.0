93ef4d9ce590e9ff64929b6f31968d85cf730dec
eric <eric.scheffer.pro@gmail.com>
2023-02-03 15:34:27 +0100

    lightBox js OK

--- public/js/lightBox.js
+++ public/js/lightBox.js
@@ -1,20 +1,33 @@
 window.onload = () => {
-   const boutonsClose = document.querySelectorAll(".close");
+   // Fenêtre modale pour afficher l'image en grand
+
    const boutonFullScreen = document.querySelector(".full-screen");
+   let modale = document.querySelectorAll(".image>.modal");
 
-
-   boutonFullScreen.addEventListener("click", clicBoutonFullScreen);
-   function clicBoutonFullScreen() {
-      let modale = document.querySelector(".image.visibility-image-on:not(.display-image-off)>.modal");
-      modale.classList.add("show");
-   }
-
-   for (let boutonClose of boutonsClose) {
+   // Quand il n'y a qu'une seule image dans la page
+   if (modale.length == 1) {
+      const boutonClose = document.querySelector(".image>.modal>.close");
+      boutonFullScreen.addEventListener("click", function () {
+         modale[0].classList.add("show");
+      });
       boutonClose.addEventListener("click", function () {
-         let modale = boutonClose.parentElement;
-         modale.classList.remove("show");
+         modale[0].classList.remove("show");
+      });
+   }
+   // Quand il y a plusieurs images
+   else {
+      const boutonsClose = document.querySelectorAll(".close");
+      boutonFullScreen.addEventListener("click", function () {
+         let modale = document.querySelector(".image.visibility-image-on:not(.display-image-off)>.modal");
+         modale.classList.add("show");
       });
 
+      for (let boutonClose of boutonsClose) {
+         boutonClose.addEventListener("click", function () {
+            let modale = boutonClose.parentElement;
+            modale.classList.remove("show");
+         });
+      }
    }
 }
 
