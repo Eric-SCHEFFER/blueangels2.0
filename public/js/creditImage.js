9d2a26400446c56397a7fce7591a80f4a8ba6d20
eric <eric.scheffer.pro@gmail.com>
2023-02-05 11:56:14 +0100

    Fenêtre credit image fonctionnelle en ouverture-fermeture sur fullPageElement

--- public/js/creditImage.js
+++ public/js/creditImage.js
@@ -1,4 +1,22 @@
-window.onload = () => {
-   
-}
+const boutonCredit = document.querySelector(".credit-image");
 
+
+boutonCredit.addEventListener("click", function (event) {
+   let creditImageContent = document.querySelector(".image.visibility-image-on:not(.display-image-off)>.credit-image-content");
+   creditImageContent.classList.toggle("show");
+   event.stopPropagation();
+});
+
+oBody.addEventListener("click", function () {
+   let creditImageContent = document.querySelector(".image.visibility-image-on:not(.display-image-off)>.credit-image-content");
+   creditImageContent.classList.remove("show");
+});
+
+
+// Préserve la fermeture de la fenetre crédit quand on clique dessus.
+const creditImageContents = document.querySelectorAll(".credit-image-content");
+for (let creditImageContent of creditImageContents) {
+   creditImageContent.addEventListener("click", function (event) {
+      event.stopPropagation();
+   });
+}
