fba5458ca3fef6e38dbb0983b340b790434fe7e4
eric <eric.scheffer.pro@gmail.com>
2023-02-21 09:04:23 +0100

    tests maintenance

--- public/index.php
+++ public/index.php
@@ -1,13 +1,23 @@
 <?php
+// Mode maintenance
+$a = "non";
+if ($a === "oui") {
+    header("Location: http://127.0.0.1:43115/blueangelsdanse-local/projet_blueangels2.0/public/.maintenanceOn.php", TRUE, 303);
+    exit();
+}
+// $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+// var_dump(dirname($_SERVER['PHP_SELF']));
+// exit;
+
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
