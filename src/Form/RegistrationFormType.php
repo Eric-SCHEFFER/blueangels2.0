259b3deb4452b67be79bf9db476aa093951cef74
eric <eric.scheffer.pro@gmail.com>
2024-08-02 15:53:07 +0000

    Style register.html.twig + ajout des contraintes (dont des regex) dans RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -19,12 +20,12 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email')
             // ->add('agreeTerms', CheckboxType::class, [
-                // 'mapped' => false,
-                // 'constraints' => [
-                // new IsTrue([
-                // 'message' => 'You should agree to our terms.',
-                // ]),
-                // ],
+            // 'mapped' => false,
+            // 'constraints' => [
+            // new IsTrue([
+            // 'message' => 'You should agree to our terms.',
+            // ]),
+            // ],
             // ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -37,10 +38,27 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mdp doit avoir au minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    new \Symfony\Component\Validator\Constraints\Regex([
+                        'pattern' => '/[&)=(?]+/',
+                        'message' => 'Doit contenir au moins l\'un de ces caractères: & ) = ( ?\')'
+                    ]),
+                    new \Symfony\Component\Validator\Constraints\Regex([
+                        'pattern' => '/[a-z]+/',
+                        'message' => 'Doit contenir au moins une minuscule'
+                    ]),
+                    new \Symfony\Component\Validator\Constraints\Regex([
+                        'pattern' => '/[A-Z]+/',
+                        'message' => 'Doit contenir au moins une majuscule'
+                    ]),
+                    new \Symfony\Component\Validator\Constraints\Regex([
+                        'pattern' => '/[0-9]+/',
+                        'message' => 'Doit contenir au moins un chiffre'
+                    ]),
+
                 ],
             ]);
     }
