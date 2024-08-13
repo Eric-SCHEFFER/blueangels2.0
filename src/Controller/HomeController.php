be292122817fc8436e42794e06f73e9f7bfded4d
eric <eric.scheffer.pro@gmail.com>
2024-08-13 16:16:11 +0200

    HomeController: Change methode getCommuniques() en getArticleForBarreNews() et utilise findOneBy

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -32,8 +32,8 @@ class HomeController extends AbstractController
     {
         // La date du jour, récupérée dans le service generateAToday
         $today = $todayGenerator->generateAToday();
-        // Les 3 derniers communiqués (dont en priorité, les épinglés)
-        $communiques = $this->getCommuniques();
+        // Le dernier article épinglé, destiné à être affiché dans la barrre de news
+        $articleForBarreNews = $this->getArticleForBarreNews();
         // Les 3 events à venir (dont en priorité, les épinglés)
         $eventsToCome = $this->get3Events($today);
         // Nbre total d'events futurs
@@ -42,59 +42,75 @@ class HomeController extends AbstractController
         $countTotalCompletedEvents = $this->eventsRepository->countTotalActifCompletedEvents($today);
         // Les 3 derniers articles
         $articles = $this->getArticles();
-        $afficherBarreCommunique = $this->optionsSiteRepository->findOneBy(['nom' => 'barre communiqués homepage']);
-
+        $afficherBarrenews = $this->optionsSiteRepository->findOneBy(['nom' => 'barre news homepage']);
         return $this->render('home.html.twig', [
             'menu_courant' => 'home',
-            'communiques' => $communiques,
+            'articleForBarreNews' => $articleForBarreNews,
             'events' => $eventsToCome,
             'epoque' => 'futur',
             'today' => $today,
             'countTotalEventsToCome' => $countTotalEventsToCome,
             'countTotalCompletedEvents' => $countTotalCompletedEvents,
             'articles' => $articles,
-            'afficherBarreCommunique' => $afficherBarreCommunique,
+            'afficherBarrenews' => $afficherBarrenews,
         ]);
     }
 
 
-
-    // BARRE COMMUNIQUÉS
+    // BARRE NEWS
 
     /**
-     * On récupère les 3 derniers articles actifs dans la catégorie Communiqués, dont en priorité les épinglés s'il y en a.
+     * On récupère le dernier article actif et épinglé
      */
-    private function getCommuniques()
+    private function getArticleForBarreNews()
     {
-        $categorie = $this->categoriesArticleRepository->findOneBy(['nom' => 'Communiqués']);
-        $pinnedCommuniques = $this->articlesRepository->findBy(
+        $articleForBarreNews = $this->articlesRepository->findOneBy(
             [
-                'categories_article' => $categorie,
                 'actif' => true,
-                'listed' => true,
                 'epingle' => true,
             ],
-            ['created_at' => "DESC"],
-            3
+            ['created_at' => "DESC"]
         );
-        $nonPinnedCommuniques = [];
-        if (count($pinnedCommuniques) < 3) {
-            // On complète avec des communiqués non-épinglés
-            $nonPinnedCommuniques = $this->articlesRepository->findBy(
-                [
-                    'categories_article' => $categorie,
-                    'actif' => true,
-                    'listed' => true,
-                    'epingle' => false,
-                ],
-                ['created_at' => "DESC"],
-                3 - count($pinnedCommuniques)
-            );
-        }
-        // On fusionne les deux tableaux $pinnedCommuniques et $nonPinnedCommuniques dans $communiques.
-        $communiques = array_merge($pinnedCommuniques, $nonPinnedCommuniques);
-        return $communiques;
+        return $articleForBarreNews;
     }
+
+
+    // // BARRE COMMUNIQUÉS
+
+    // /**
+    //  * On récupère les 3 derniers articles actifs dans la catégorie Communiqués, dont en priorité les épinglés s'il y en a.
+    //  */
+    // private function getCommuniques()
+    // {
+    //     $categorie = $this->categoriesArticleRepository->findOneBy(['nom' => 'Communiqués']);
+    //     $pinnedCommuniques = $this->articlesRepository->findBy(
+    //         [
+    //             'categories_article' => $categorie,
+    //             'actif' => true,
+    //             'listed' => true,
+    //             'epingle' => true,
+    //         ],
+    //         ['created_at' => "DESC"],
+    //         3
+    //     );
+    //     $nonPinnedCommuniques = [];
+    //     if (count($pinnedCommuniques) < 3) {
+    //         // On complète avec des communiqués non-épinglés
+    //         $nonPinnedCommuniques = $this->articlesRepository->findBy(
+    //             [
+    //                 'categories_article' => $categorie,
+    //                 'actif' => true,
+    //                 'listed' => true,
+    //                 'epingle' => false,
+    //             ],
+    //             ['created_at' => "DESC"],
+    //             3 - count($pinnedCommuniques)
+    //         );
+    //     }
+    //     // On fusionne les deux tableaux $pinnedCommuniques et $nonPinnedCommuniques dans $communiques.
+    //     $communiques = array_merge($pinnedCommuniques, $nonPinnedCommuniques);
+    //     return $communiques;
+    // }
 
 
 
