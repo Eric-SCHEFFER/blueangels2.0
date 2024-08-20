bd5982920392680b70999ac2e692d6028e1b9523
eric <eric.scheffer.pro@gmail.com>
2024-08-20 22:40:16 +0200

    admin/preview/cardArticle.html.twig: Debut construction html

--- src/Controller/AdminArticlePreviewController.php
+++ src/Controller/AdminArticlePreviewController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticlesRepository;
 
 class AdminArticlePreviewController extends AbstractController
 {
+    public function __construct(
+        ArticlesRepository $articlesRepository
+    ) {
+        $this->articlesRepository = $articlesRepository;
+    }
+
     #[Route('admin/preview/card_article/{id}', name: 'admin.preview.card.article')]
-    public function loadCardArticlePreview(): Response
+    public function loadArticle($id, ArticlesRepository $articlesRepository): Response
     {
+        $article = $this->articlesRepository->find($id);
         return $this->render('admin/preview/cardArticle.html.twig', [
-            'controller_name' => 'AdminArticlePreviewController',
+            'article' => $article,
         ]);
     }
 }
