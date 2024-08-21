17b3d0c7c4d6d470baf92c00b8485f6b34b81821
eric <eric.scheffer.pro@gmail.com>
2024-08-21 11:04:37 +0200

    Apercus carte article et article fullpage OK

--- src/Controller/AdminFullPageArticlePreviewController.php
+++ src/Controller/AdminFullPageArticlePreviewController.php
@@ -6,21 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticlesRepository;
+use App\Repository\ImagesArticleRepository;
+
 
 class AdminFullPageArticlePreviewController extends AbstractController
 {
     public function __construct(
-        ArticlesRepository $articlesRepository
+        ArticlesRepository $articlesRepository,
+        ImagesArticleRepository $imagesArticleRepository
     ) {
         $this->articlesRepository = $articlesRepository;
+        $this->imagesArticleRepository = $imagesArticleRepository;
     }
 
     #[Route('admin/preview/fullPageArticle/{id}', name: 'admin.preview.fullPageArticle')]
     public function loadArticle($id, ArticlesRepository $articlesRepository): Response
     {
         $article = $this->articlesRepository->find($id);
+        $images = $this->imagesArticleRepository->findBy(['articles' => $article]);
+
         return $this->render('admin/preview/fullPageArticle.html.twig', [
             'article' => $article,
+            'images' => $images
         ]);
     }
 }
