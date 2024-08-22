5a2276ff8442cdfdf85f02422a2b3f60a13395e8
eric <eric.scheffer.pro@gmail.com>
2024-08-22 15:57:21 +0200

    FEAT: Apercu event OK

--- src/Controller/AdminCardEventPreviewController.php
+++ src/Controller/AdminCardEventPreviewController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EventsRepository;
+use App\Service\TodayGenerator;
 
 class AdminCardEventPreviewController extends AbstractController
 {
@@ -16,11 +17,23 @@ class AdminCardEventPreviewController extends AbstractController
     }
 
     #[Route('admin/preview/card_event/{id}', name: 'admin.preview.card.event')]
-    public function loadEvent($id, EventsRepository $eventsRepository): Response
+    public function loadEvent($id, EventsRepository $eventsRepository, TodayGenerator $todayGenerator): Response
     {
         $event = $this->eventsRepository->find($id);
+
+        // On récupère la date du jour, que l'on peut changer dans la classe
+        $today = $todayGenerator->generateAToday();
+
+        // On determine si l'event est futur ou passé
+        if ($event->getDateEvent()->format('Y-m-d') >= $today->format('Y-m-d')) {
+            $epoque = "futur";
+        } else
+            $epoque = "passe";
+
         return $this->render('admin/preview/cardEvent.html.twig', [
             'event' => $event,
+            'epoque' => $epoque,
+            'today' => $today
         ]);
     }
 }
--- src/Controller/AdminFullPageEventPreviewController.php
+++ src/Controller/AdminFullPageEventPreviewController.php
@@ -5,29 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ArticlesRepository;
-use App\Repository\ImagesArticleRepository;
+use App\Repository\EventsRepository;
+use App\Repository\ImagesEventRepository;
+use App\Service\TodayGenerator;
+
 
 
 class AdminFullPageEventPreviewController extends AbstractController
 {
     public function __construct(
-        ArticlesRepository $articlesRepository,
-        ImagesArticleRepository $imagesArticleRepository
+        EventsRepository $eventsRepository,
+        ImagesEventRepository $imagesEventRepository
     ) {
-        $this->articlesRepository = $articlesRepository;
-        $this->imagesArticleRepository = $imagesArticleRepository;
+        $this->eventsRepository = $eventsRepository;
+        $this->imagesEventRepository = $imagesEventRepository;
     }
 
-    #[Route('admin/preview/fullPageArticle/{id}', name: 'admin.preview.fullPageArticle')]
-    public function loadArticle($id, ArticlesRepository $articlesRepository): Response
+    #[Route('admin/preview/fullPageEvent/{id}', name: 'admin.preview.fullPageEvent')]
+    public function loadEvent($id, EventsRepository $eventsRepository, TodayGenerator $todayGenerator): Response
     {
-        $article = $this->articlesRepository->find($id);
-        $images = $this->imagesArticleRepository->findBy(['articles' => $article]);
+        $event = $this->eventsRepository->find($id);
+        $images = $this->imagesEventRepository->findBy(['event' => $event]);
 
-        return $this->render('admin/preview/fullPageArticle.html.twig', [
-            'article' => $article,
-            'images' => $images
+        // On récupère la date du jour, que l'on peut changer dans la classe
+        $today = $todayGenerator->generateAToday();
+
+        // On determine si l'event est futur ou passé
+        if ($event->getDateEvent()->format('Y-m-d') >= $today->format('Y-m-d')) {
+            $epoque = "futur";
+        } else
+            $epoque = "passe";
+
+        return $this->render('admin/preview/fullPageEvent.html.twig', [
+            'event' => $event,
+            'images' => $images,
+            'epoque' => $epoque,
+            'today' => $today,
         ]);
     }
 }
