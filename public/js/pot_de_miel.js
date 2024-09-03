523f781fc511978ea89e0027ce3511a84d37a783
eric <eric.scheffer.pro@gmail.com>
2024-09-03 15:51:49 +0200

    rien

--- public/js/pot_de_miel.js
+++ public/js/pot_de_miel.js
@@ -1,9 +1,5 @@
-
 // On masque la div grand-parent du champ leurre input
 let champInput = document.querySelector('#contact_email');
 let blocLigneGrandParent = champInput.parentNode.parentNode;
 
-blocLigneGrandParent.style.visibility = ("hidden");
-blocLigneGrandParent.style.height = ("0");
-blocLigneGrandParent.style.width = ("0");
-
+blocLigneGrandParent.style.display = ("none");
