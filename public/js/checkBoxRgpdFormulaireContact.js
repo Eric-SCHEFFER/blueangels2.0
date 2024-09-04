dbc86c03f8f4f6dd979f0886c7857a57a72d6f59
eric <eric.scheffer.pro@gmail.com>
2024-09-04 21:04:57 +0200

    Add message alert rgpd no checked when button is grised in contact form

--- public/js/checkBoxRgpdFormulaireContact.js
+++ public/js/checkBoxRgpdFormulaireContact.js
@@ -1,22 +1,30 @@
 window.onload = () => {
    let caseACocher = document.getElementById("accordDonneesPerso");
    let boutonSubmit = document.getElementById("contact_envoyer");
+   let messageCocherRgpd = document.getElementById("cocherRgpdErrorId");
 
    caseACocher.checked = false;
-   boutonSubmit.disabled = true;
+   // boutonSubmit.disabled = true;
 
    // Bascule: Active/désactive le bouton submit selon que la case d'acceptation de collecte des données soit cochée ou non.
    caseACocher.addEventListener('change', function () {
       if (this.checked) {
          boutonSubmit.className = "buttonEnabled";
-         //document.getElementById("contact_envoyer").setAttribute("enabled", "enabled");
-         boutonSubmit.disabled = false;
+         if (messageCocherRgpd.classList.contains("displayBlock")) {
+            messageCocherRgpd.classList.remove("displayBlock");
+         }
 
       } else {
          boutonSubmit.className = "buttonDisabled";
-         boutonSubmit.disabled = true;
+      }
+   });
 
-
+   // Quand On clique sur le bouton envoyer, et qu'il est disabled, on affiche un message rouge disant qu'il faut d'abord cocher la case
+   boutonSubmit.addEventListener('click', function (e) {
+      if (this.classList.contains("buttonDisabled")) {
+         e.preventDefault();
+         messageCocherRgpd.classList.add("displayBlock");
+         e.stopPropagation
       }
    });
 }
