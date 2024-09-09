9a831dd600211096ac337d81db1ba82c3776d778
eric <eric.scheffer.pro@gmail.com>
2024-09-09 16:15:30 +0200

    Remove_err_display_in_contactForm_when_action_in_input_field: OK

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -5,7 +5,8 @@ window.onload = () => {
    let contactForm = document.getElementsByName("contact");
    let datas = document.querySelector("#datas");
    let isFormValid = datas.dataset.dataformvalid; // Variable de twig passée par l'attribut de données data-*
-
+   const lignesInputs = document.querySelectorAll(".constraints-messages");
+   // console.log(lignesInputs);
 
    caseACocher.checked = false;
    // boutonSubmit.disabled = true;
@@ -37,6 +38,7 @@ window.onload = () => {
 
 
    if (isFormValid) {
+      // Premier affichage du formulaire, donc, pas d'erreurs
       // Antispam robots: On détecte l'event du premier changement dans le formulaire (début d'écriture, cochage de case)
       contactForm[0].addEventListener('change', function () {
          // On envoie le timeStamp de l'event dans le champ caché
@@ -45,14 +47,34 @@ window.onload = () => {
          { once: true }
       );
    } else {
+      // Retours d'affichage du formulaire, avec au moins 1 erreur. Les champs restent remplis.
       // On envoie dès le chargement de la page, le timeStamp de l'event dans le champ caché.
       // Explications:
-      // Quand on est sur un retour d'erreur du formulaire,
+      // Quand on est sur un retour d'erreur du formulaire, comme les champs sont déjà remplis,
       // pour éviter le risque d'une soumission trop rapide, si par ex 1 seul champ est en erreur,
       // le décompte du temps de soumission risque de ne commencer qu'après le cochage de la case du RGPD,
-      // si, après avoir réinscrit les bonne données dans le champ concerné, il clique de suite
-      // sur la case rgpd, et sur envoi très rapidement (on peut le faire en moins d'1s)
+      // si, après avoir réinscrit les bonnes données dans le champ concerné, l'utilisateur clique de suite
+      // sur la case rgpd, et sur envoi très rapidement (on peut le faire en moins d'1s).
+      // La détection du temps de soumission côté client riquerait de faire un faux positif
       document.getElementById('contact_beginTimeClientSide').value = Math.round(Date.now() / 1000);
+
+      // Dès qu'on commence à saisir (dans input ou textArea), on efface le message d'erreur éventuellement présent en dessous, lors d'un retour de formulaire
+      // On cible chaque ligne
+      for (let ligneInputs of lignesInputs) {
+         const divs = ligneInputs.querySelectorAll("div");
+         // On cible chaque div dans la ligne
+         for (const div of divs) {
+            // Si présente, on cible l'ul dans la div, et on la supprime (c'est elle qui contenait la li du message d'erreur)
+            if (div.querySelector("ul")) {
+               div.querySelector("ul").nextElementSibling.addEventListener('beforeinput', function () {
+                  div.querySelector("ul").remove();
+               },
+                  { once: true }
+               );
+            }
+         }
+      }
    }
+
 
 }
