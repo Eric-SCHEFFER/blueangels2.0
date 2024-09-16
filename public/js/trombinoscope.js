1ca86fac458d3a4c40e75d1cc3556be26039bf11
eric <eric.scheffer.pro@gmail.com>
2024-09-16 11:07:30 +0200

    Pilotage en js des balises html dans la classe infos-membre de trombinoscope.html.twig dans trombinoscope.js

--- public/js/trombinoscope.js
+++ public/js/trombinoscope.js
@@ -5,10 +5,11 @@ const cards = document.querySelectorAll(".card");
 const modal = document.querySelector("#modal");
 const modalContent = modal.firstElementChild;
 const close = modalContent.firstElementChild;
-// const infosMembre = document.querySelector(".infos-membre");
-const dtTags = document.querySelector(".infos-membre").querySelectorAll("dt");
-const ddTags = document.querySelector(".infos-membre").querySelectorAll("dd");
+const infosMembre = document.querySelector(".infos-membre");
+// const dtTags = infosMembre.querySelectorAll("dt");
+// const ddTags = infosMembre.querySelectorAll("dd");
 const darkerLayer = document.querySelector(".darker-layer");
+
 
 for (let card of cards) {
    card.addEventListener("click", function (event) {
@@ -21,28 +22,46 @@ for (let card of cards) {
       modalContent.querySelector('.fonction').innerText = datas.dataset.fonction;
       modalContent.querySelector('.description').innerText = datas.dataset.description;
 
-      // Contenu avec icônes fontawesome (dans div class infos-membre)
+      // TODO: On ajoute les balises dl,dt et dd, et leurs contenus avec icônes fontawesome (dans div class infos-membre)
       if (datas.dataset.telephone !== "") {
+         let dlTelephone = document.createElement("dl");
+         dlTelephone.classList.add("telephone");
+         let dtTelephone = document.createElement("dt");
+         let ddTelephone = document.createElement("dd");
          let iTelephone = document.createElement("i");
          iTelephone.classList.add("fa-solid", "fa-phone")
-         modalContent.querySelector('.telephone>dt').appendChild(iTelephone);
-         modalContent.querySelector('.telephone>dd').innerText = datas.dataset.telephone;
+         infosMembre.appendChild(dtTelephone);
+         infosMembre.appendChild(ddTelephone);
+         dtTelephone.appendChild(iTelephone);
+         ddTelephone.innerText = datas.dataset.telephone;
       }
       if (datas.dataset.email !== "") {
+         let dlEmail = document.createElement("dl");
+         dlEmail.classList.add("email");
+         let dtEmail = document.createElement("dt");
+         let ddEmail = document.createElement("dd");
          let iEmail = document.createElement("i");
          iEmail.classList.add("fa-solid", "fa-envelope")
-         modalContent.querySelector('.email>dt').appendChild(iEmail);
-         modalContent.querySelector('.email>dd').innerText = datas.dataset.email;
+         infosMembre.appendChild(dtEmail);
+         infosMembre.appendChild(ddEmail);
+         dtEmail.appendChild(iEmail);
+         ddEmail.innerText = datas.dataset.email;
       }
       if (datas.dataset.facebook !== "") {
+         let dlFacebook = document.createElement("dl");
+         dlFacebook.classList.add("telephone");
+         let dtFacebook = document.createElement("dt");
+         let ddFacebook = document.createElement("dd");
          let iFacebook = document.createElement("i");
          iFacebook.classList.add("fa-brands", "fa-facebook-f")
-         modalContent.querySelector('.facebook>dt').appendChild(iFacebook);
-         modalContent.querySelector('.facebook>dd').innerText = datas.dataset.facebook;
+         infosMembre.appendChild(dtFacebook);
+         infosMembre.appendChild(ddFacebook);
+         dtFacebook.appendChild(iFacebook);
+         ddFacebook.innerText = datas.dataset.facebook;
       }
 
       modalContent.querySelector('.hook img').src = card.querySelector("img").src;
-      
+
       // On affiche la modale
       modal.classList.add("show", "restore-click", "restore-scroll");
       darkerLayer.classList.add("to-dark");
@@ -55,12 +74,7 @@ oBody.addEventListener("click", function () {
    modal.classList.remove("show", "restore-click", "restore-scroll");
    darkerLayer.classList.remove("to-dark");
    oBody.classList.remove("no-click-no-scroll");
-   for (let dtTag of dtTags) {
-      dtTag.innerHTML = "";
-   }
-   for (let ddTag of ddTags) {
-      ddTag.innerHTML = "";
-   }
+   infosMembre.innerHTML = "";
 });
 
 modalContent.addEventListener("click", function (event) {
@@ -72,12 +86,7 @@ close.addEventListener("click", function () {
    modal.classList.remove("show", "restore-click", "restore-scroll");
    darkerLayer.classList.remove("to-dark");
    oBody.classList.remove("no-click-no-scroll");
-   for (let dtTag of dtTags) {
-      dtTag.innerHTML = "";
-   }
-   for (let ddTag of ddTags) {
-      ddTag.innerHTML = "";
-   }
+   infosMembre.innerHTML = "";
 });
 
 // Fermeture de la modale avec echap
@@ -87,12 +96,7 @@ document.addEventListener("keyup", function (event) {
          modal.classList.remove("show", "restore-click", "restore-scroll");
          darkerLayer.classList.remove("to-dark");
          oBody.classList.remove("no-click-no-scroll");
-         for (let dtTag of dtTags) {
-            dtTag.innerHTML = "";
-         }
-         for (let ddTag of ddTags) {
-            ddTag.innerHTML = "";
-         }
+         infosMembre.innerHTML = "";
       }
    }
 });
