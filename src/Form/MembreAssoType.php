39443a5bd11d74b24fe5c39bc1beae3c8e3d2f9f
eric <eric.scheffer.pro@gmail.com>
2024-09-25 11:08:09 +0200

    contraints_MembreAssoType: OK

--- src/Form/MembreAssoType.php
+++ src/Form/MembreAssoType.php
@@ -6,11 +6,18 @@ use App\Entity\MembresAsso;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Repository\MembresAssoRepository;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 
 class MembreAssoType extends AbstractType
@@ -23,10 +30,110 @@ class MembreAssoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('fonction')
-            ->add('description');
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'required' => True,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez compléter ce champ',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Minimum {{ limit }} caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('nom', TextType::class, [
+                'required' => True,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez compléter ce champ',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Minimum {{ limit }} caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('fonction', TextType::class, [
+                'required' => True,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez compléter ce champ',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Minimum {{ limit }} caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('email', EmailType::class, [
+                'required' => False,
+                'constraints' => [
+                    new Email([
+                        'message' => 'l\'adresse email n\'est pas valide'
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('facebook', UrlType::class, [
+                'required' => False,
+                'constraints' => [
+                    new Url([
+                        'message' => 'Ce n\'est pas une url valide'
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => False,
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Minimum {{ limit }} caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ])
+
+            ->add('actif', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Actif',
+            ])
+
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez compléter ce champ',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Minimum {{ limit }} caractères',
+                        'max' => 4096,
+                        'maxMessage' => 'Maximum {{ limit }} caractères'
+                    ])
+                ]
+            ]);
 
         // On ajoute l'inputFile (photo) uniquement s'il n'y a pas déjà une image en base
         if (empty($this->getPhoto($options))) {
@@ -53,15 +160,6 @@ class MembreAssoType extends AbstractType
                 ]
             ]);
         }
-
-        $builder->add('email')
-            ->add('facebook')
-            ->add('telephone')
-
-            ->add('actif', CheckboxType::class, [
-                'required' => false,
-                'label' => 'Actif',
-            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
